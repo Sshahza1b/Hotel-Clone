d598421bb87e08c93d463412cb8092e7d1b19a97
Shahzaib <129912416+Sshahza1b@users.noreply.github.com>
2023-06-09 23:16:07 +0500

    add reactjs
    add files

---
+++ Hotel/Hotel/src/Components/Testimonial/Tdata.js
@@ -0,0 +1,46 @@
+const Tdata = [
+  {
+    id: 1,
+    desc: "Lorem ipsum dolor sit amet consectetur ipiscing elit amet consectetur piscing elit consectetur adipiscing elit sed et eletum. orem ipsum dolor sit amet consectetur adipiscing elit amet consectetur piscing elit amet consectetur adipiscing elit sed et eletum nulla eu placerat felis etiam tincidunt orci lacus id varius dolor fermum sit amet.",
+    name: "Shahzaib",
+    profile: "/images/profile/profile1.jpg",
+    post: "Developer",
+  },
+  {
+    id: 2,
+    desc: "Lorem ipsum dolor sit amet consectetur ipiscing elit amet consectetur piscing elit consectetur adipiscing elit sed et eletum. orem ipsum dolor sit amet consectetur adipiscing elit amet consectetur piscing elit amet consectetur adipiscing elit sed et eletum nulla eu placerat felis etiam tincidunt orci lacus id varius dolor fermum sit amet.",
+    name: "Ahsan",
+    profile: "/images/profile/profile2.jpg",
+    post: "Designer",
+  },
+  {
+    id: 3,
+    desc: "Lorem ipsum dolor sit amet consectetur ipiscing elit amet consectetur piscing elit consectetur adipiscing elit sed et eletum. orem ipsum dolor sit amet consectetur adipiscing elit amet consectetur piscing elit amet consectetur adipiscing elit sed et eletum nulla eu placerat felis etiam tincidunt orci lacus id varius dolor fermum sit amet.",
+    name: " MANSOOR",
+    profile: "/images/profile/profile3.jpg",
+    post: "Support-Marketing",
+  },
+  {
+    id: 4,
+    desc: "Lorem ipsum dolor sit amet consectetur ipiscing elit amet consectetur piscing elit consectetur adipiscing elit sed et eletum. orem ipsum dolor sit amet consectetur adipiscing elit amet consectetur piscing elit amet consectetur adipiscing elit sed et eletum nulla eu placerat felis etiam tincidunt orci lacus id varius dolor fermum sit amet.",
+    name: "SImon",
+    profile: "/images/profile/profile4.jpg",
+    post: "CEO",
+  },
+  {
+    id: 5,
+    desc: "Lorem ipsum dolor sit amet consectetur ipiscing elit amet consectetur piscing elit consectetur adipiscing elit sed et eletum. orem ipsum dolor sit amet consectetur adipiscing elit amet consectetur piscing elit amet consectetur adipiscing elit sed et eletum nulla eu placerat felis etiam tincidunt orci lacus id varius dolor fermum sit amet.",
+    name: "DAVID",
+    profile: "/images/profile/profile5.jpg",
+    post: "Consultant",
+  },
+  {
+    id: 6,
+    desc: "Lorem ipsum dolor sit amet consectetur ipiscing elit amet consectetur piscing elit consectetur adipiscing elit sed et eletum. orem ipsum dolor sit amet consectetur adipiscing elit amet consectetur piscing elit amet consectetur adipiscing elit sed et eletum nulla eu placerat felis etiam tincidunt orci lacus id varius dolor fermum sit amet.",
+    name: "MILLER",
+    profile: "/images/profile/profile6.jpg",
+    post: "Founder",
+  },
+]
+
+export default Tdata
